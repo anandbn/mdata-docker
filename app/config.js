737cd48db8bf38fc494619f773e95b90f0ae9cd6
Anand Narasimhan <anarasimhan@salesforce.com>
2019-12-28 21:34:48 -0500

    Added apoc,algo plugins and updated to use neo4j.conf

--- app/config.js
+++ app/config.js
@@ -1,12 +1,12 @@
 module.exports = [
     {
         "DeveloperName": "Profiles",
-        "Enabled__c": false,
+        "Enabled__c": true,
         "Additional_Configuration__c": null
     },
     {
         "DeveloperName": "RecordTypes",
-        "Enabled__c": false,
+        "Enabled__c": true,
         "Additional_Configuration__c": null
     },
     {
@@ -16,42 +16,42 @@ module.exports = [
     },
     {
         "DeveloperName": "WorkflowRules",
-        "Enabled__c": false,
+        "Enabled__c": true,
         "Additional_Configuration__c": null
     },
     {
         "DeveloperName": "FormulaFields",
-        "Enabled__c": false,
+        "Enabled__c": true,
         "Additional_Configuration__c": null
     },
     {
         "DeveloperName": "ListViews",
-        "Enabled__c": false,
+        "Enabled__c": true,
         "Additional_Configuration__c": null
     },
     {
         "DeveloperName": "PicklistValues",
-        "Enabled__c": false,
+        "Enabled__c": true,
         "Additional_Configuration__c": null
     },
     {
         "DeveloperName": "PublicGroups",
-        "Enabled__c": false,
+        "Enabled__c": true,
         "Additional_Configuration__c": null
     },
     {
         "DeveloperName": "ReportTypes",
-        "Enabled__c": false,
+        "Enabled__c": true,
         "Additional_Configuration__c": null
     },
     {
         "DeveloperName": "Roles",
-        "Enabled__c": false,
+        "Enabled__c": true,
         "Additional_Configuration__c": null
     },
     {
         "DeveloperName": "ValidationRules",
-        "Enabled__c": false,
+        "Enabled__c": true,
         "Additional_Configuration__c": null
     },
     {
@@ -61,29 +61,29 @@ module.exports = [
     },
     {
         "DeveloperName": "Layouts",
-        "Enabled__c": false,
+        "Enabled__c": true,
         "Additional_Configuration__c": null
     },
     {
         "DeveloperName": "Reports",
-        "Enabled__c":false,
+        "Enabled__c":true,
         "Additional_Configuration__c": {
             "reportsTimePeriodDays": 120
         }
     },
     {
         "DeveloperName": "WorkflowFieldUpdates",
-        "Enabled__c": false,
+        "Enabled__c": true,
         "Additional_Configuration__c": null
     },
     {
         "DeveloperName": "ApexTriggers",
-        "Enabled__c": false,
+        "Enabled__c": true,
         "Additional_Configuration__c": null
     },
     {
         "DeveloperName": "ApexClasses",
-        "Enabled__c": false,
+        "Enabled__c": true,
         "Additional_Configuration__c": null
     }
 ]
